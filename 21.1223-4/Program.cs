97df6ffcfc8f3e0fef9ced0af1e0717bc6d43e06
inwardness <trimethylfentanyl@gmail.com>
2023-12-25 22:30:09 +0300

    new lessons 21.1223

---
+++ 21.1223-4/Program.cs
@@ -0,0 +1,21 @@
+﻿//Напишите программу, которая выводит третью слева цифру 
+//заданного числа или сообщает, что третьей цифры нет.
+
+Console.WriteLine("введите  число");
+int num = Convert.ToInt32(Console.ReadLine());
+int number = num;
+if(num < 0) num*=-1;
+if(num >= 100)
+{
+    while(num > 999)
+    {
+        num = num / 10;// num /=10;
+
+    }
+    int result = num % 10;
+    Console.WriteLine($"{result}");
+}
+else
+{
+    Console.WriteLine($"{number} третьей цифры нет");
+}
