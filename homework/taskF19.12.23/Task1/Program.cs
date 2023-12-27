89934bdefc1812640c1cf7b82834039c865ed19e
inwardness <trimethylfentanyl@gmail.com>
2023-12-27 22:30:49 +0300

    add types values in code

--- homework/taskF19.12.23/Task1/Program.cs
+++ homework/taskF19.12.23/Task1/Program.cs
@@ -1,1 +1,10 @@
-﻿Console.WriteLine("Yello, Sir!");
+﻿//Console.WriteLine("Yello, Sir!");
+
+int value = 42;
+Console.WriteLine(value);
+double d = 5.55;
+char c = '!';
+bool b = true;
+Console.WriteLine(d);
+Console.WriteLine(c);
+Console.WriteLine(b);
