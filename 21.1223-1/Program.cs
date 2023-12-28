745de0a26251baa0bffd928f66756b21e1d2303c
inwardness <trimethylfentanyl@gmail.com>
2023-12-28 21:20:25 +0300

    modified:   ../../../.gitignore 	modified:   ../../../21.1223-1/Program.cs

--- 21.1223-1/Program.cs
+++ 21.1223-1/Program.cs
@@ -9,7 +9,7 @@ if(num >= 100 && num <= 999)
 {
     int firstDigit = num / 100; //256/2
     int lastDigit = num % 10; //256=250+6
-    int result = firstDigit * 10 + lastDigit;
+    int result = firstDigit * 10  + lastDigit;
     Console.WriteLine($"{num}=>{result}");
 }
 
