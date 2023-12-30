7c8001894eb9bd89de87f9d414583d8efb427f58
inwardness <trimethylfentanyl@gmail.com>
2023-12-30 21:46:38 +0300

    modified:   .gitignore

---
+++ homework/taskF21.12.23/Task1/Program.cs
@@ -0,0 +1,11 @@
+﻿int firstNum = Console.ReadLine(Convert.ToInt32());
+int secondNum = Console.ReadLine(Convert.ToInt32());
+
+if( firstNum == secondNum * secondNum ) 
+{
+    Console.WriteLine($"a = {firstNum},b = {secondNum} => b is a quard a");
+}
+else
+{
+    Console.WriteLine($"a = {firstNum},b = {secondNum} => b is not a quard a");    
+}
