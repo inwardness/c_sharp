37ba0e722747f0f098f047f61bfbce9168dfddc1
inwardness <trimethylfentanyl@gmail.com>
2024-01-02 11:55:09 +0300

    modified:   homework/taskF28.12.23/Task3/Program.cs 	modified:   homework/taskF28.12.23/Task4/Program.cs

--- homework/taskF28.12.23/Task3/Program.cs
+++ homework/taskF28.12.23/Task3/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿     
+Console.WriteLine("Enter value from 10 to 99:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+while( number < 10 || number > 99)
+{
+    Console.WriteLine("value must preset from 10 to 99");
+    Console.WriteLine("Enter value from 10 to 99:");
+    number = Convert.ToInt32(Console.ReadLine());
+}
+
+if( number > 10 || number < 99)
+{
+    int higherDigit = number/10;
+    int lowerDigit = number%10;
+
+    if( lowerDigit > higherDigit)
+    {
+        Console.WriteLine($"{lowerDigit} - higher value of number{ number} ");
+    }
+    else
+    {
+        Console.WriteLine($"{higherDigit} - higher value of number{ number} ");
+    }
+}
--- homework/taskF28.12.23/Task4/Program.cs
+++ homework/taskF28.12.23/Task4/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.WriteLine("Enter number:");
+int num = Convert.ToInt32(Console.ReadLine());
+
+while(num > 0)
+    {
+    int digit = num%10;
+    Console.ReadLine();
+    int x = Console.Write;
+    Console.Write($",{digit}");
+    num/=10;
+    }
