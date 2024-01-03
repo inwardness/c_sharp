bd15b72980fd321372e0b39d053f243d13bded86
inwardness <trimethylfentanyl@gmail.com>
2024-01-03 13:47:38 +0300

    modified:   homework/taskF21.12.23/Task1/Program.cs

--- homework/taskF21.12.23/Task1/Program.cs
+++ homework/taskF21.12.23/Task1/Program.cs
@@ -1,13 +1,16 @@
-
+﻿//Напишите программу, которая на вход
+//принимает два целых числа и проверяет,
+//является ли первое число квадратом второго.
+Console.WriteLine("checking whether the first number is the square of the second.\n ");
 Console.WriteLine("enter first number:\n ");    
-int firstNum = Console.ReadLine(Convert.ToInt32());
+int firstNum = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("enter second number:\n ")
+Console.WriteLine("enter second number:\n ");
 int secondNum = Convert.ToInt32(Console.ReadLine());
 
 if( firstNum == secondNum * secondNum ) 
 {
-    Console.WriteLine($"a = {firstNum},b = {secondNum} => b is a quard a");
+    Console.WriteLine($"a = {firstNum},b = {secondNum} => a is a quard b");
 }
 else
 {
