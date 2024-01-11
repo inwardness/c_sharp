d88bd7d717be23b4d170f40fb9a902976be9a025
inwardness <trimethylfentanyl@gmail.com>
2024-01-11 22:42:50 +0300

    Task4

--- 11.01.24/Task3/Program.cs
+++ 11.01.24/Task3/Program.cs
@@ -1,1 +1,9 @@
-﻿
+﻿int[] arr = new int[] { 1, 3, 2, 4, 2, 3};
+int[] arr2 = new int[arr.Length/2];
+
+for (int i = 0; i < arr.Length / 2; i++)
+{
+    arr2[i] = arr[i] * arr[arr.Length - i -1];
+    Console.Write($"{arr2[i]} ");
+}
+
