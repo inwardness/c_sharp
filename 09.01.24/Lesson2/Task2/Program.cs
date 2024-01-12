c93df0dc447c99aac7acd99f0b6a5fe158714516
inwardness <trimethylfentanyl@gmail.com>
2024-01-12 16:45:01 +0300

    new file:   09.01.24/Lesson2/Task2/Program.cs 	new file:   09.01.24/Lesson2/Task3/Program.cs 	new file:   09.01.24/Lesson2/Task4/Program.cs 	new file:   "09.01.24/\320\234\320\265\321\202\320\276\320\264\320\270\321\207\320\272\320\260. \320\234\320\260\321\201\321\201\320\270\320\262\321\213.pdf"

---
+++ 09.01.24/Lesson2/Task2/Program.cs
@@ -0,0 +1,12 @@
+﻿int N = 10;
+int[] arr = new int[N];
+int i = 0;
+while (i < arr.Length)
+{
+    arr[i] = i + 1;
+    //Console.WriteLine(arr[i]); в столбец
+    Console.Write(arr[i]); //в строку
+    Console.Write(" ");
+    i = i + 1;
+
+}
---
+++ 09.01.24/Lesson2/Task3/Program.cs
@@ -0,0 +1,11 @@
+﻿int N = 10;
+int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+int i =0 ;
+while (i < N)
+{
+    if (arr[i] % 2 == 0)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    i = i + 1;
+}
