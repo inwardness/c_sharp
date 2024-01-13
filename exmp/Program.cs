62edb3e97d2421d93edadfccbaeb2c391a65c31f
inwardness <trimethylfentanyl@gmail.com>
2024-01-13 04:20:53 +0300

    3 task to auto-test

---
+++ exmp/Program.cs
@@ -0,0 +1,20 @@
+﻿class Program
+    {
+        static int Sum(int number)
+        {
+            int result = 0;
+            for (int i = 1; i <= number; i++)
+            {
+                result += i;
+                
+            }
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            int number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(Sum(number));
+
+        }
+    }
--- homework/11.01.24/Task1/Program.cs
+++ homework/11.01.24/Task1/Program.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿//Задайте одномерный массив из 10 целых чисел от 1 до 100.
+// Найдите количество элементов массива, значения которых лежат в отрезке [10,90].
+//Начальные условия:
+//int[] array = {1, 5, 10, 20, 30, 40, 99, 4, 90, 3}; // Создание массива
+//Выводится: 5
+
+using System;
 using System.Linq;
 
 //Тело класса будет написано студентом. Класс обязан иметь статический метод PrintResult()
@@ -8,32 +14,41 @@ class UserInputToCompileForTest
     // numbers - массив, в котором ведется подсчет
     // minRange - минимальная граница диапазона
     // maxRange - максимальная граница диапазона
+
+
     public static int CountItemsRange(int[] numbers, int minRange, int maxRange)
     {
-        //Введите сюда свое решение
-        int[] number = new int[] { 10, 30, 2, 40, 99, 52, 100, 35};
-        minRange =10;
-        maxRange =90;
-        int arr =0;
-        for (int i = 0; i < numbers.Length; i++)
+        int count = 0;
+        for (int i = 0; i < numbers.Length; ++i)
+        {
             if (numbers[i] >= minRange && numbers[i] <= maxRange)
             {
-                 arr = arr + 1;
+                count = count + 1;
+                //Console.Write(numbers[i]+" ");
             }
-
-    return arr;
-
+        }
+        //Введите сюда свое решение
+        Console.WriteLine($"Вывод:{count}");
+       return count;
+       
     }
 
     public static void PrintResult(int[] array)
     {
-
         //Введите сюда свое решение
-        PrintResult(array);
+        
+        int minRange = 10;
+        int maxRange = 90;
+        int[] numbers = new int[10];
+        for (int i = 0; i < array.Length; ++i)
+        {
+            numbers[i] = array[i];
+        }
+        CountItemsRange(numbers, minRange, maxRange);
+        //Console.WriteLine($"{count}");
     }
-
+    
 }
-
 
 //Не удаляйте и не меняйте класс Answer!
 class Answer
@@ -42,24 +57,20 @@ class Answer
     {
         int[] array;
 
-
         if (args.Length >= 1)
         {
             // Объединяем все аргументы командной строки в одну строку
             string joinedArgs = string.Join(" ", args);
 
             // Разделяем строку по запятой с пробелом и преобразуем в массив целых чисел
-            array = joinedArgs.Split(", ")
-                                  .Select(int.Parse)
-                                  .ToArray();
+            array = joinedArgs.Split(", ").Select(int.Parse).ToArray();
 
             // Теперь arr содержит преобразованные в целые числа из командной строки
-
         }
         else
         {
             // Если аргументов на входе нет
-            array = new int[] { 1, 5, 10, 20, 30, 40, 99, 4, 90, 3 }; // Создание массива
+            array = new int[] { 1, 5, 10, 2, 30, 4, 99, 4, 9, 3 }; // Создание массива
         }
         UserInputToCompileForTest.PrintResult(array);
     }
