ab989840cd8ab3208baa00fd355ae66352b62f07
inwardness <trimethylfentanyl@gmail.com>
2024-01-16 20:59:52 +0300

    add task on 15.01.24

---
+++ 15.01.24/Task1/Program.cs
@@ -0,0 +1,60 @@
+﻿
+//Задайте одномерный массив, заполненный случайными числами
+//Определите количество простых чисел в этом массиве
+//Примеры
+//[1 34 193] => 2
+//[4 341 9521 13] => 3
+
+
+int size = 5;
+
+int[] arr = GetArrayRndInt(size, max);
+PrintArray(arr);
+
+int CntPrimeNumbers = CountPrimeNumbers(arr);
+Console.WriteLine($"=>{CntPrimeNumbers}");
+
+int[] GetArrayRndInt(int size, int max)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(100);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+
+}
+
+int CountPrimeNumbers(int[] array)
+{
+    int count = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        //bool IsPrimeNumber = IsPrimeNumber(array[i]);
+        if (IsPrimeNumber(array[i]))
+                 ++count;
+        
+    }
+            return count;
+}
+
+bool IsPrimeNumber(int num)
+{
+    for (int i = 2; i <= Math.Sqrt(num); i++)
+    {
+        if (num % i == 0) return false;
+    }
+    return true;
+}
+
+
--- exmp/Program.cs
+++ exmp/Program.cs
@@ -1,20 +1,8 @@
-﻿class Program
+﻿internal class Program
+{
+    private static void Main(string[] args)
     {
-        static int Sum(int number)
-        {
-            int result = 0;
-            for (int i = 1; i <= number; i++)
-            {
-                result += i;
-                
-            }
-            return result;
-        }
-
-        static void Main(string[] args)
-        {
-            int number = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine(Sum(number));
-
-        }
+        Console.Write("nop");
+        Console.WriteLine("green_yard");
     }
+}
