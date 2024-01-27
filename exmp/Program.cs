77aef4087a83846e3953ce37db843500d9d1ca03
inwardness <trimethylfentanyl@gmail.com>
2024-01-27 20:56:52 +0300

    add 18.01.24 mess

--- exmp/Program.cs
+++ exmp/Program.cs
@@ -1,7 +1,19 @@
-﻿
-string str = "1sG,:0"
-    foreach (char ch in str)
+﻿using System;
+Console.WriteLine("Enter a massive:");
+int[,] arr = new int[3, 4];
+for (int i = 0; i < 3; ++i)
 {
-    Console.WriteLine(ch + " - " + char.getUnicodeCategory(ch));\
+    for (int j = 0; j < 4; j++)
+    {
+        Console.Write("mas[" + i + "," + j + "]: ");
+        arr[i, j] = int.Parse(Console.Read());
+    }
+}
 
+for (int i = 0; i < 3.; ++i)
+{
+    for (int j = 0; j < 4; j++)
+    {
+        Console.Write(arr[i, j] + " ");
+    }
 }
