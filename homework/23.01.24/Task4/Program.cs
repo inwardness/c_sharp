665cbd48a76bdfacdedddbd8612c060592767ee9
inwardness <trimethylfentanyl@gmail.com>
2024-01-27 10:48:01 +0300

    add 15.01 24.01

---
+++ homework/23.01.24/Task4/Program.cs
@@ -0,0 +1,30 @@
+﻿
+//Задайте с строку,состоящую из слов,разделённых пробелами.
+//Сформировать строку,в которой слова расположены в обратном порядке.
+//В полученной строке слова должны быть так же разделены пробелами.
+//"Hello my world"  => "world my Hello"
+Console.WriteLine("Enter text:");
+string str = Console.ReadLine();
+
+ReverseWords(str);
+
+static void ReverseWords(string str)
+{
+    string[] words = str.Split(' ');
+    string resultStr = string.Empty;
+
+
+    for (int i = words.Length - 1; i >= 0; i--)
+    {
+        if (i > 0)
+        {
+            resultStr += words[i] + " ";
+        }
+        else
+        {
+            resultStr += words[i];
+        }
+    }
+
+    Console.WriteLine(resultStr);
+}
